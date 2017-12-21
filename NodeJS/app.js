cda0329588365773a9aec815fb0edfd6ab368021
Igor Matos <igormatos.developer@gmail.com>
2017-12-21 16:08:51 -0200

    Adding profile.ejs to project

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,13 +1,24 @@
 let express = require('express');
 let app = express();
 
+app.set('view engine', 'ejs');
+
+
 app.get('/', function (req, resp) {
-    resp.send('This is a homepage');
+    resp.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function (req, resp) {
-    resp.send('This is the contact page');
+    resp.sendFile(__dirname + '/contact.html');
 });
 
+app.get('/profile/:name', function (req, res) {
+    let data = {
+        age: 19,
+        job: 'ninja',
+        hobbies: ['eating', 'fighting', 'fishing']
+    };
+    res.render('profile', {person: req.params.name, data: data});
+});
 
 app.listen(3000);
