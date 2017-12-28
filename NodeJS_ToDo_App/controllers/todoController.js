57f979794b86c67c5cee7048a6fd77ce14eaa46e
Igor Matos <igormatos.developer@gmail.com>
2017-12-28 17:21:16 -0200

    Finishing todoController and communication with database, now I'm going to synchronize get, post and delete methods with MongoDB

--- NodeJS_ToDo_App/controllers/todoController.js
+++ NodeJS_ToDo_App/controllers/todoController.js
@@ -1,6 +1,22 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+//connect to the database
+mongoose.connect('mongodb://igor.matos:id11402210@ds135547.mlab.com:35547/todo');
+
+//create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+var itemOne = Todo({item: 'Get Flowers'}).save(function (err) {
+    if (err) throw err;
+    console.log('item saved');
+});
+
+
 var data = [{item: 'Make bed'}, {item: 'Walk dog'}, {item: 'Play games'}];
 
 var urlEncodedParser = bodyParser.urlencoded({extended: false});
