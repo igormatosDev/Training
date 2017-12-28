5bad837e15dbcc1a3bc3603f24a3435f37352353
Igor Matos <igormatos.developer@gmail.com>
2017-12-27 23:22:47 -0200

    Starting the project, there was the lessons 1 and 2, there are more 4 lessons until I finish it.

---
+++ NodeJS_ToDo_App/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var ejs = require('ejs');
+var todoController = require('./controllers/todoController');
+
+//set up templates engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire controllers
+todoController(app);
+
+//listen to port
+app.listen(3000);
+console.log('You are listening to port 3000');
